295d02d489417d11cd361461e6b06b0ec8b17952
harsimran <developer.erharsimran@gmail.com>
2019-01-21 00:07:10 -0800

    harsimranlist

---
+++ list.py
@@ -0,0 +1,35 @@
+list=["hello",'3','5','6']   # 1 create list files
+>>> print list
+list.append("new")           # 2 add new variable 
+>>> print list
+list[1]=("hiii")             # 3 change the second item
+>>> print list
+['hello', 'hiii', '5', '6', 'new']
+list = ["apple", "banana", "cherry"]     # 4 list vise output
+for x in list:
+  print(x)
+  list=["apple","mango"]                  # 5 check list
+>>> if "apple" in list
+print ("yes")
+list = ["apple", "banana", "cherry"]       # 6 list integer count
+print(len(list))
+cars = ['Ford', 'BMW', 'Volvo']            # 7 sort
+cars.sort() 
+print (cars)
+list = ['apple', 'banana', 'cherry']       # 8 remove variable
+list.remove("banana") 
+fruits = ['apple', 'banana', 'cherry', 'orange']   # 9 listcopy
+>>> x = fruits.copy()
+>>> print(x)
+fruits = ['apple', 'banana', 'cherry']             # 10 index
+>>> x = fruits.index("cherry")
+>>> print (x)
+fruits = ['apple', 'banana', 'cherry']             # 11 reverse
+fruits.reverse() 
+print (fruits)
+list = ["apple", "banana", "cherry"]               # 12 for loop
+for x in list:
+  print(x)
+list = ["apple", "banana", "cherry"]               #13  delete variable in list
+del list[0]
+print(thislist) 
